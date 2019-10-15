a0214fda5f9db2fee012170ec6d421a8a30c9808
arussell36 <andrewrussell036@gmail.com>
2019-10-15 18:31:58 -0500

    pseudocode

---
+++ script.js
@@ -0,0 +1,32 @@
+// GLOBAL VARIABLES //
+
+
+
+
+
+
+
+// UTILITY FUNCTIONS //
+
+    // RECEIVE RAW DATA FROM API AND PARSE //
+        // render the parsed data //
+
+
+
+
+
+
+// EVENT FUNCTIONS //
+ 
+    // SEARCH FOR CITY (SEARCH BUTTON CLICK ACTION) //
+        // send the city name to the openweather API //
+        // set the weather info to the object returned //
+
+
+
+
+
+
+// INITIALIZE START STATE //
+
+    // CHECK LOCAL STORAGE FOR PREVIOUS HISTORIES AND RENDER //
